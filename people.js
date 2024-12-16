3cf1638ff0cbf4306912bb8d67056c0ce94e7756
matteodragotto <matteodrag8@gmail.com>
2024-12-16 15:19:22 +0100

    [feat] people.js

--- people.js
+++ people.js
@@ -6,4 +6,11 @@ const hobbies = require('./modules/hobbies')
 console.log(fullName('matteo', 'dragotto'));
 console.log(hobbies('Nuoto', 'Corsa', 'Calcio'));
 
+function namesAndHobbies () {
+  return {fullname: fullName('matteo', 'dragotto').name, hobbies: hobbies('Nuoto', 'Corsa', 'Calcio').hobbies }
+}
+
+console.log(namesAndHobbies());
+
+
 
